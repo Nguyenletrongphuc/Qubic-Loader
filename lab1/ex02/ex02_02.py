6020e7e3ff3bfe0cc7314638b0b2a721a90cbf7a
Nguyenletrongphuc <168173454+Nguyenletrongphuc@users.noreply.github.com>
2025-02-28 16:28:19 +0700

    Add files via upload

---
+++ lab1/ex02/ex02_02.py
@@ -0,0 +1,3 @@
+bankinh = float(input("Nhap ban kinh cua hinh tron: "))
+dientich = 3.14 * (bankinh ** 2)
+print("Dien tich la", dientich)
---
+++ lab1/ex02/ex02_10.py
@@ -0,0 +1,4 @@
+def daonguocchuoi(chuoi):
+    return chuoi[::-1]
+input_string = input("moi nhap chuoi : ")
+print("chuoi dao nguoc la: ",daonguocchuoi(input_string))
